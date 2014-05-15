24e12cad77632d133fb3c0c9af2ef56d92d181b9
Rob <schae234@umn.edu>
2014-05-15 08:08:49 -0500

    added tmp_file method to camoco class

--- src/Camoco.py
+++ src/Camoco.py
@@ -4,6 +4,7 @@ import sqlite3 as lite
 import os as os
 import time as time
 import sys
+import tempfile
 
 class Camoco(object):
     def __init__(self,basedir="~/.camoco"):
@@ -16,6 +17,7 @@ class Camoco(object):
                 os.mkdir(os.path.join(self.basedir,"logs"))
                 os.mkdir(os.path.join(self.basedir,"databases"))
                 os.mkdir(os.path.join(self.basedir,"analyses"))
+                os.mkdir(os.path.join(self.basedir,"tmp"))
             except Exception as e:
                 print("[CAMOCO]",time.ctime(), '-', *args,file=sys.stderr)
         self.log_file = open(os.path.join(self.basedir,"logs","logfile.txt"),'w')
@@ -31,12 +33,15 @@ class Camoco(object):
     def database(self,dbname):
         # return a connection if exists
         return lite.connect(self._resource("databases",str(dbname)+".db"))
+    def tmp_file(self):
+        # returns a handle to a tmp file
+        return tempfile.NamedTemporaryFile(dir=os.path.join(self.basedir,"tmp"))
+        
 
     def available_datasets(self):
         cur=self.database("camoco").cursor()
         return cur.execute("SELECT rowid,* FROM datasets;").fetchall()
-        
-               
+    
 
     def add_dataset(self,name,description='',type=''):
         con = self.database("camoco")
