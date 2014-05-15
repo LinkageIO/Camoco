e0b1639a65154c696628adb1659df71bf87cd155
Rob <schae234@umn.edu>
2014-05-15 08:10:26 -0500

    updated import class to be intelligent about insertions. Added method to build indices

--- src/HapMap.py
+++ src/HapMap.py
@@ -36,10 +36,10 @@ class HapMapBuilder(Camoco):
         self.log("Importing VCF: {}",filename)
         with open(filename,'r') as INVCF:
             genotype_rows = []
-            cur = self.db.cursor()
+            snp_rows = []
+            snpRowID = 1 # 
+            con = self.db
             for i,line in enumerate(INVCF):
-                if i % 100000 == 0:
-                    self.log("Importing VCF line {}",i)
                 line = line.strip()
                 if line.startswith("##"):
                     next
@@ -47,31 +47,52 @@ class HapMapBuilder(Camoco):
                     # we are at the header section, extract accessions here
                     (chrom,pos,id,ref,alt,qual,fltr,info,fmt,*accessions) = line.split()
                     for accession in accessions:
-                        cur.execute('''
+                        con.execute('''
                             INSERT INTO accessions VALUES('{}');
                         '''.format(accession)
                         )
                 else:
                     # we are at records, insert snp line by line but bulk insert genotypes
                     (chrom,pos,snpid,ref,alt,qual,fltr,info,fmt,*genotypes) = line.split()
-                    cur.execute(''' 
-                        INSERT INTO snps VALUES(?,?,?,?,?,?);
-                    ''',(chrom,pos,snpid,ref,alt,qual)
-                    )
-                    snpid = cur.lastrowid
+                    snp_rows.append((chrom,pos,snpid,ref,alt,qual))
                     GT_ind = fmt.split(":").index("GT") # we need to find the GT field
-                    for j,genotype in enumerate(genotypes):
+                    for accessionRowID,genotype in enumerate(genotypes):
                         (allele1,allele2) = genotype.split(":")[GT_ind].replace("|","/").split("/")
-                        genotype_rows.append((snpid,j+1,allele1,allele2))
-            # Bulk import the genotype values
-            cur.executemany('''
-                INSERT INTO genotypes VALUES(?,?,?,?)
-            ''',genotype_rows)
-            self.db.commit()
+                        genotype_rows.append((snpRowID,accessionRowID+1,allele1,allele2))
+                    snpRowID += 1 # <- Do NOT fuck this up, genotypes need to reference correct SNPId
+                if i % 1000000 == 0 and i > 0:
+                    self.log(" {} records reached dumping data",i)
+                    start_time = time.time()
+                    # Bulk import the genotype values
+                    self.log("\tInserting snps...")
+                    con.executemany(''' 
+                        INSERT INTO snps (chromosome,position,id,alt,ref,qual)
+                         VALUES(?,?,?,?,?,?)
+                    ''',snp_rows)
+                    self.log("\tInserting genotypes...")
+                    con.executemany('''
+                        INSERT INTO genotypes VALUES(?,?,?,?)
+                    ''',genotype_rows)
+                    genotype_rows = []
+                    snp_rows = []
+                    con.commit()
+                    self.log("Total execution time for block: {} seconds",time.time()-start_time)
         self.log("Import finished")
                     
+    def _create_indices(self):
+        self.db.execute('''
+            CREATE INDEX IF NOT EXISTS genoAccID ON genotypes (accessionRowID);
+        ''')
+        self.db.execute('''
+            CREATE INDEX IF NOT EXISTS snpChrom ON snps (chromosome);
+        ''')
+        self.db.execute('''
+            CREATE INDEX IF NOT EXISTS snpPos ON snps (position);
+        ''')
 
     def _create_tables(self):
+        self.db.execute("PRAGMA page_size = 8192;")
+        self.db.execute("PRAGMA cache_size = 10000;")
         self.db.execute(''' 
             CREATE TABLE IF NOT EXISTS snps (
                 chromosome INTEGER NOT NULL,
