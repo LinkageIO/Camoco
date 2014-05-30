04242b2c14a2e8cf4587ca6781a77df97cce694f
Rob <schae234@umn.edu>
2014-05-30 13:12:24 -0500

    Added len, str, and repr methods to Chrom class

--- src/Chrom.py
+++ src/Chrom.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python
 
+import numpy as np
+from Locus import Locus
+
 class Chrom(object):
     def __init__(self,id,length):
         self.id = id
         self.length = length
-    
+
     def rQTL(self,length):
         ''' returns a random QTL within the chromosome '''
         start = np.random.randint(0,self.length)
         while start+length > self.length:
             start = np.random.randint(0,self.length)
-        return QTL(chrom=self.id,start=start,end=start+length,id="RQTL-{}".format(length))
+        return Locus(chrom=self.id,start=start,end=start+length,id="RQTL-{}".format(length))
+
     def rLocus(self):
         ''' returns a random Locus from within the chromosome '''
         pos = np.random.randint(0,self.length)
         return Locus(chrom=self.id,start=pos,end=pos,id='rLocus-chr{}:{}'.format(self.id,pos))
+
+    def __len__(self):
+        return self.length
+
+    def __str__(self):
+        return "Chromosome {}: {} bp".format(self.id,self.length)
+
+    def __repr__(self):
+        return str(self)
 
 
