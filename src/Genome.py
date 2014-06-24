432f838d1e8f75616ec03d6d02fa4be60fb2314f
Rob <schae234@umn.edu>
2014-06-24 17:29:42 -0500

    Added clarification in Genome class to randomization in locus and SNP

--- src/Genome.py
+++ src/Genome.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import numpy as np
 
 class Genome(object):   
     def __init__(self,id,chroms=list()):
@@ -11,12 +12,12 @@ class Genome(object):
         ''' returns a random chromosome object from genome '''
         rindex = np.random.randint(0,len(self.chroms))
         return self.chroms[rindex]
-    def rQTL(self,length):
+    def rLocus(self,length):
         ''' returns a random QTL of specified lengths '''
-        return self.rChrom().rQTL(length)
-    def rLocus(self):
+        return self.rChrom().rLocus(length)
+    def rSNP(self):
         ''' returns a random 'SNP' from the genome '''
-        return self.rChrom().rLocus()
+        return self.rChrom().rSNP()
 
     def __repr__(self):
         return "\n".join(map(str,self.chroms))
