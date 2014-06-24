651d945c645ff1eece64b7008b599c18da306e37
Rob <schae234@umn.edu>
2014-06-24 17:33:51 -0500

    added useful abstraction toolbox for bootstrapping and also make initialization of class smarter

--- src/Locus.py
+++ src/Locus.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-
+from collections import defaultdict
 
 class Locus(object):
     def __init__(self, chrom, start, end, id=None ,gene_build='5b', organism='Zea'):
-        self.chrom = int(chrom)
-        self.start = int(start)
-        self.end = int(end)
+        self.chrom = chrom
+        try:
+            self.start = int(start)
+        except TypeError as e:
+            self.start = None
+        try:
+            self.end = int(end)
+        except TypeError as e:
+            self.end = None
         self.id = id
         self.gene_build = gene_build
         self.organism = organism
@@ -14,6 +20,16 @@ class Locus(object):
     def stop(self):
         ''' because im an idiot ''' 
         return self.end
+
+    def __eq__(self,locus):
+        if (self.chrom == locus.chrom and
+            self.start == locus.start and
+            self.end == locus.end and
+            self.gene_build == locus.gene_build and
+            self.organism == locus.organism):
+            return True
+        else:
+            return False
 
     def __contains__(self,locus):
         if (locus.chrom == self.chrom and
@@ -27,13 +43,13 @@ class Locus(object):
         ''' inclusive length of locus '''
         return self.stop - self.start + 1
 
-    def __cmp__(self,snp):
-        return self.start - snp-start
+    def __cmp__(self,locus):
+        return self.start - locus.start
 
-    def __lt__(self,snp):
-        return self.start < snp.start    
-    def __gt__(self,snp):
-        return self.start > snp.start
+    def __lt__(self,locus):
+        return self.start < locus.start    
+    def __gt__(self,locus):
+        return self.start > locus.start
     def __sub__(self,other):
         return self.start - other.start
     def __str__(self):
@@ -50,7 +66,7 @@ class Locus(object):
 class SNP(Locus):
     def __init__(self, chrom, pos, id=None ,gene_build='5b', organism='Zea'):
         self.pos = int(pos)
-        super(self.__class__,self).__init__(chrom,pos,pos,id,gene_build,organism)
+        super(self.__class__,self).__init__(str(chrom),int(pos),int(pos),id,gene_build,organism)
        
 
 class Gene(Locus):
@@ -64,8 +80,48 @@ class QTL(Locus):
             self.id = "QTL-chr{}:{}-{}".format(chrom,start,end)
         else:
             self.id = id
-        super().__init__(chrom,start,end,self.id)
+        super().__init__(str(chrom),start,end,self.id)
     def __str__(self):
         return "{}".format(self.id)
 
 
+# -----------------------End of Class Definitions -------------------------------------------
+
+# Useful abstractions for analyses
+def bootstrap(loci,genome):
+    ''' Returns a non-overlapping, randoms set of equal 
+        size loci from the genome '''
+    random_loci = [genome.rLocus(len(locus)) for locus in loci]
+    if non_overlapping(random_loci):
+        return random_loci
+    else:
+        return bootstrap(loci,genome)
+
+def num_overlap(loci1,loci2,unique=False):
+    ''' Returns the number of loci in list 1 which overlap list 2 
+        If unique is True, function return the number of unique loci 
+        overlapping between lists '''
+    total = defaultdict(int)
+    for x in loci1:
+        for y in loci2:
+            if x in y:
+                total[str(y)] += 1
+    if unique:
+        return len(total)
+    else:
+        return sum(total.values())
+
+def non_overlapping(loci_list):
+    ''' Return True if loci do not overlap with one another
+        otherwise returns False if any two loci overlap '''
+    if num_overlap(loci_list,loci_list) > len(loci_list):
+        return False
+    else:
+        return True    
+
+def significant_overlap(loci1,loci2,genome,iterations=1000,unique=False):
+    ''''Returns the number of bootstrapped overlaps larger than or equal to loci1 in loci2
+        for i iterations '''
+    emp_overlap = num_overlap(loci1,loci2,unique=unique)
+    bs = [ num_overlap(loci1,bootstrap(loci2,genome),unique=unique) for x in range(iterations)]
+    return sum([x >= emp_overlap for x in bs])/iterations
