423891e5cf69359655d0de07213de8382c2b26d0
Rob <schae234@umn.edu>
2014-07-31 16:11:43 -0500

    Moved Memoization decorator function to tools file.

--- src/Tools.py
+++ src/Tools.py
@@ -1,7 +1,27 @@
 import os
-from itertools import chain
+import time
 import re
+import functools
+
+from itertools import chain
 from Locus import *
+
+def memoize(obj):
+    cache = obj.cache = {}
+
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        # This wraps the calling of the memoized object
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
+
+def log(msg,*args,basedir="~/.camoco"):
+    if os.path.exists(os.path.expanduser(os.path.join(basedir,'logs','logfile.txt'))):
+        print("[CAMOCO]",time.ctime(), '-', self.name, '-', msg.format(*args),file=self.log_file)
+    print("[CAMOCO]",time.ctime(), '-', self.name, '-', msg.format(*args),file=sys.stderr)
 
 def ext(filename):
     return os.path.join(os.path.expanduser("~/MetaboloCOB/"+filename))
@@ -17,8 +37,3 @@ def B73_eq_Mo17(snp,HM):
         return True
     else:
         return False
-def Bootrap(snps,networks,reference,ontologies):
-    pass 
-
-
-
