c917e30d7317e5e7c6450b29a52b33a70b25bb45
Rob <schae234@umn.edu>
2014-08-25 11:23:10 -0500

    Added Term Class to ontology

--- src/Ontology.py
+++ src/Ontology.py
@@ -1,29 +1,101 @@
 #!/usr/bin/python
 
 from Camoco import Camoco
+from RefGen import RefGen
+from Locus import SNP
+
 from collections import defaultdict
 from pandas import DataFrame
 from scipy.stats import hypergeom
 import re
 
+class Term(object):
+    def __init__(self,id,name='',type='',desc='',gene_list=None,snp_list=None):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.desc = desc
+        self.gene_list = set()
+        self.snp_list = set()
+        if gene_list:
+            self.gene_list = set(gene_list)
+        if snp_list:
+            self.snp_list = set(snp_list)
+    def __len__(self):
+        return len(self.gene_list)
+
+    def summary(self):
+        print("\n".join([self.id,self.name,self.type,self.desc]))
+
+    def add_gene(self,gene):
+        self.gene_list.add(gene)
+
+    def add_snp(self,snp):
+        self.snp_list.add(snp) 
+
+    def __str__(self):
+        return "Term: {}, {} genes, {} SNPs".format(self.id,len(self.gene_list),len(self.snp_list))
+
+    def __repr__(self):
+        return str(self.id)
+
 class Ontology(Camoco):
+    ''' An Ontology is just a collection of terms. Each term is just a collection of genes. 
+        Sometimes terms are related or nested within each other, sometimes not. Simple enough.  
+    '''
     def __init__(self,name,basedir="~/.camoco"):
         super(self.__class__,self).__init__(name,type='Ontology',basedir=basedir)
+        self.refgen = RefGen(self.refgen)
 
-    def term_genes(self,term):
-        return [x[0] for x in self.db.cursor().execute("SELECT gene from gene_terms WHERE term = ?",(term,)).fetchall()]
+    def __getitem__(self,item):
+        return self.term(item) 
+
+    def __len__(self):
+        return self.db.cursor().execute("SELECT COUNT(*) FROM terms;").fetchone()[0]
+
+    def term(self,term_id):
+        ''' retrieve a term by name '''
+        try:
+            id,name,type,desc = self.db.cursor().execute(
+                'SELECT id,name,type,desc from terms WHERE id = ?',(term_id,)
+            ).fetchone()
+            term_genes = list(self.refgen.from_ids([ x[0] for x in self.db.cursor().execute(
+                'SELECT gene from gene_terms WHERE term = ?',(id,)).fetchall()
+            ]))
+            term_snps = [SNP(*x) for x in self.db.cursor().execute(
+                'SELECT chrom,pos FROM snp_terms WHERE term = ?',(id,)
+            ).fetchall()]
+            return Term(id,name,type,desc,gene_list=term_genes,snp_list=term_snps)
+        except TypeError as e: # Not in database
+            return None
+
+    def term_ids(self,like="%"):
+        return [self.term(x[0]) for x in self.db.cursor().execute('SELECT id FROM terms WHERE id LIKE ?',(like,)).fetchall()]
+
+    def term_names(self,like="%"):
+        return [self.term(x[0]) for x in self.db.cursor().execute('SELECT id FROM terms WHERE name LIKE ?',(like,)).fetchall()]
+
+    def iter_terms(self):
+        for id,name,type,desc in self.db.cursor().execute("SELECT id,name,type,desc FROM terms"):
+            term_genes = list(self.refgen.from_ids([ x[0] for x in self.db.cursor().execute(
+                'SELECT gene from gene_terms WHERE term = ?',(id,)).fetchall()
+            ]))
+            term_snps = [SNP(*x) for x in self.db.cursor().execute(
+                'SELECT chrom,pos FROM snp_terms WHERE term = ?',(id,)
+            ).fetchall()]
+            yield Term(id,name,type,desc,term_genes,term_snps)
 
     def terms(self):
-        return self.db.cursor().execute("SELECT * from terms").fetchall()
+        return list(self.iter_terms())
 
-    def enrichment(self,gene_list,pval_cutoff=0.05):
+    def enrichment(self,gene_list,pval_cutoff=0.05,gene_filter=None):
         # extract possible terms for genes
         cur = self.db.cursor()
         terms = [ x[0] for x in cur.execute(
             '''SELECT DISTINCT(term) FROM gene_terms 
             WHERE gene IN ('{}');'''.format("','".join([x.id for x in gene_list]))
         )]
-        # computer hypergeometric for each term
+        # compute hypergeometric for each term
         enrichment = []
         for id in terms:
             try:
@@ -33,16 +105,19 @@ class Ontology(Camoco):
             genes_in_term = [x[0] for x in cur.execute(
                 '''SELECT gene FROM gene_terms WHERE term = ?''',(id,))
             ]
+            if gene_filter:
+                genes_in_term = [gene for gene in genes_in_term if gene in gene_filter]
             num_genes_in_term = len(genes_in_term)
             overlap = set(genes_in_term).intersection(set([x.id for x in gene_list]))
             num_genes_total, = cur.execute('SELECT COUNT(DISTINCT(gene)) FROM gene_terms;').fetchone()
             pval = hypergeom.sf(len(overlap)-1,num_genes_total,num_genes_in_term,len(gene_list))
+            term_genes = ",".join(overlap)
             enrichment.append(
-                (id,name,pval,num_genes_in_term,len(overlap),len(gene_list),num_genes_total,type,desc)
+                (id,name,pval,num_genes_in_term,len(overlap),len(gene_list),num_genes_total,type,term_genes,desc)
             )
         try:
             enrichment = DataFrame(enrichment,
-                columns = ['TermID','Name','pval','LenTerm','LenOverlap','LenList','LenTotal','Type','Desc']
+                columns = ['TermID','Name','pval','LenTerm','LenOverlap','LenList','LenTotal','Type','TermGenes','Desc']
             ).sort('pval',ascending=True)
             enrichment.index = enrichment.TermID
         except ValueError as e:
@@ -50,14 +125,27 @@ class Ontology(Camoco):
             return DataFrame()
         return enrichment[enrichment.pval <= pval_cutoff]
 
-    def term(self,id):
-        return self.db.cursor().execute('SELECT * FROM terms WHERE id = ?',(id,)).fetchall()
-            
+    def summary(self):
+        return "Ontology: name:{} - desc: {} - contains {} terms".format(self.name,self.desc,len(self))
+
 
 class OntologyBuilder(Camoco):
-    def __init__(self,name,description='',basedir="~/.camoco"):
+    def __init__(self,name,description,refgen,basedir="~/.camoco"):
         super().__init__(name,description=description,type="Ontology",basedir=basedir)
+        self._global('refgen',refgen.name)
         self._create_tables()
+
+    def add_term(self,term):
+        ''' This will add a single term to the ontology '''
+        cur = self.db.cursor()
+        cur.execute('BEGIN TRANSACTION')
+        # Add info to the terms tabls
+        cur.execute('INSERT OR REPLACE INTO terms (id,name,type,desc) VALUES (?,?,?,?)',(term.id,term.name,term.type,term.desc))
+        for gene in term.gene_list:
+            cur.execute('INSERT OR REPLACE INTO gene_terms (gene,term) VALUES (?,?)',(gene.id,term.id))
+        for snp in term.snp_list:
+            cur.execute('INSERT OR REPLACE INTO snp_terms (chrom,pos,term) VALUES (?,?,?)',(snp.chrom, snp.pos, term.id))
+        cur.execute('END TRANSACTION')
 
     def import_gene_terms(self, filename, gene_col=1, term_col=2, term_filter=".*",
         gene_filter=".*", skip=0, sep="\t"):
@@ -196,4 +284,12 @@ class OntologyBuilder(Camoco):
                 gene TEXT,
                 term TEXT
             );            
+        ''')
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS snp_terms (
+                chrom TEXT,
+                pos TEXT,
+                id TEXT,
+                term TEXT
+            );
         ''')
