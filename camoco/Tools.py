9f553df01250581816c387111e223e8a5e6ed891
Rob <schae234@umn.edu>
2014-10-20 18:04:39 -0500

    removed snp from string method which was moved to a classmethod

--- camoco/Tools.py
+++ camoco/Tools.py
@@ -19,17 +19,13 @@ def memoize(obj):
         return cache[key]
     return memoizer
 
+
 def log(msg,*args):
     print("[LOG]",time.ctime(), '-', msg.format(*args),file=sys.stderr)
 
 def ext(filename):
     return os.path.join(os.path.expanduser("~/MetaboloCOB/"+filename))
-def snp_from_str(strs):
-    snps = []
-    for s in strs:
-        m = re.match("S(\d\d?)_(\d+)_.*",s)
-        snps.append(SNP(m.group(1),m.group(2)))
-    return snps
+
 def B73_eq_Mo17(snp,HM):
     genotypes = HM.genotypes(snp,accessions=['B73','MO17'])
     if genotypes[0] == genotypes[1]:
