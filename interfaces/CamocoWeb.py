0ca166ecc18028686a41b1a565797bfb600e6de7
Rob <schae234@umn.edu>
2014-10-22 17:43:53 -0500

    Added global degree field to annotation table

--- interfaces/CamocoWeb.py
+++ interfaces/CamocoWeb.py
@@ -6,6 +6,9 @@ import sys
 import json
 from math import isinf
 
+networks = {x:co.COB(x) for x in ['ROOT','SAM','PAN']}
+ZM = co.RefGen('Zm5bFGS')
+
 @app.route('/')
 def index():
     return app.send_static_file("index.html")
@@ -21,13 +24,28 @@ def available_datasets(type=None,*args):
 @app.route("/Ontology/Terms/<path:term_name>")
 def Ontology_Terms(term_name):
     return jsonify({
-        "data": [ (term.id,term.name,len(term.snp_list),len(term.gene_list)) for term in co.Ontology(term_name).iter_terms()]
+        "data": [ (term.id,term.name,
+                    len(term.snp_list),
+                    len(term.gene_list),
+                    len(set(term.flanking_genes(networks['ROOT'].refgen)).union([gene for gene in term.gene_list if gene in networks['ROOT'].refgen]))
+                ) for term in co.Ontology(term_name).iter_terms()]
     })
 
 @app.route('/api/Annotations/<network_name>/<ontology_name>/<term_name>',methods=['GET','POST'])
 def Annotations(network_name,ontology_name,term_name):
     # Retrieve SNPs from 
-    gene_annots = co.Annotation('Func')[request.args.get('genes').split(',')]
+    cob = co.COB(network_name)
+    term = co.Ontology(ontology_name)[term_name]
+    if len(term.gene_list) == 0:
+        genes = term.flanking_genes(cob.refgen,gene_limit=4,window_size=100000)
+    else:
+        genes = term.gene_list
+        genes = term.gene_list
+    gene_annots = co.Annotation('Func')[genes]
+    for net in networks.values():
+        gene_annots.insert(5,'In {}'.format(net.name), ['true' if gene in net else 'false' for gene in genes])
+    gene_annots.insert(5,'Term SNPs',["\n".join([snp.summary() for snp in sorted(term.flanking_snps(gene))]) for gene in genes])
+    gene_annots.insert(5,'Global Degree',[str(cob.global_degree(gene)) for gene in genes])
     return jsonify({
         'data' : list(gene_annots.itertuples(index=False))
     })
@@ -36,8 +54,14 @@ def Annotations(network_name,ontology_name,term_name):
 def COB_network(network_name,ontology,term):
     try:
         net = {}
-        subnet = co.COB(network_name).subnetwork(co.Ontology(ontology)[term].gene_list,min_distance=100000)
-        net['nodes'] = [ {'data':{'id':str(x), 'index':i }} for i,x in enumerate(set(subnet.source).union(subnet.target)) ]
+        cob = co.COB(network_name)
+        term = co.Ontology(ontology)[term]
+        if len(term.gene_list) == 0:
+            genes = term.flanking_genes(cob.refgen,gene_limit=4,window_size=100000)
+        else:
+            genes = term.gene_list
+        subnet = cob.subnetwork(genes,min_distance=100000)
+        net['nodes'] = [ {'data':{'id':str(x.id), 'index':i, 'gdegree':cob.global_degree(x) }} for i,x in enumerate(genes) ]
         net['edges'] = [
             {
                 'data':{
@@ -48,8 +72,8 @@ def COB_network(network_name,ontology,term):
                 }
             } for source,target,score,distance in subnet.itertuples(index=False)
         ]
+        # 
         return jsonify(net)
-        return(str(network_name)+str(ontology_term))
     except Exception as e:
         return 500 
 
@@ -60,5 +84,4 @@ def fix_inf(val):
         return val
 
 if __name__ == "__main__":
-    app.debug=True
     app.run('0.0.0.0',8080)
