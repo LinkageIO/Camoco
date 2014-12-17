06bc43690757bfcdabebceff6143bd5fb45b01d7
Rob <schae234@umn.edu>
2014-12-17 12:04:10 -0600

    Overhauled the log object. Its now a class with different levels of logging. Also added some plotting things for terms. WE might want to move these into term class evenutally.

--- camoco/Tools.py
+++ camoco/Tools.py
@@ -4,8 +4,14 @@ import time
 import re
 import functools
 
+from termcolor import colored,cprint
 from itertools import chain
 from camoco.Locus import *
+
+import camoco as co
+import matplotlib.pylab as pylab
+import numpy as np
+import pandas as pd
 
 def memoize(obj):
     cache = obj.cache = {}
@@ -20,8 +26,16 @@ def memoize(obj):
     return memoizer
 
 
-def log(msg,*args):
-    print("[LOG]",time.ctime(), '-', msg.format(*args),file=sys.stderr)
+class log(object):
+    def __init__(self,msg=None,*args,color='green'):
+        if msg is not None:
+            print(colored(" ".join(["[LOG]",time.ctime(), '-', msg.format(*args)]),color=color),file=sys.stderr)
+    @classmethod
+    def warn(cls,msg,*args):
+        cls(msg,*args,color='red')
+
+    def __call__(self,msg,*args,color='green'):
+        print(colored(" ".join(["[LOG]",time.ctime(), '-', msg.format(*args)]),color=color),file=sys.stderr)
 
 def ext(filename):
     return os.path.join(os.path.expanduser("~/MetaboloCOB/"+filename))
@@ -33,3 +47,39 @@ def B73_eq_Mo17(snp,HM):
     else:
         return False
 
+def plot_local_global_degree(term,filename=None,bootstraps=1):
+    ROOT = co.COB("ROOT")
+    RZM = ROOT.refgen # use root specific for bootstraps
+    hood = ROOT.neighborhood(term.flanking_genes(RZM))
+    bshood = pd.concat([ROOT.neighborhood(term.bootstrap_flanking_genes(RZM)) for _ in range(0,bootstraps)])
+    pylab.clf()
+    pylab.scatter(bshood['local'],bshood['global'],alpha=0.05)
+    pylab.scatter(hood['local'],hood['global'],c='r')
+    pylab.xlabel('Local Degree')
+    pylab.ylabel('Global Degree')
+    pylab.title('{} Locality'.format(term.id))
+    if filename is None:
+        filename = "{}_locality.png".format(term.id)
+    pylab.savefig(filename)
+
+def plot_local_vs_cc(term,filename=None,bootstraps=1):
+    RZM = co.COB('ROOT').refgen # use root specific for bootstraps
+    pylab.clf()
+    for _ in range(0,bootstraps):
+        graph = co.COB('ROOT').graph(term.bootstrap_flanking_genes(RZM))
+        degree = np.array(graph.degree())
+        cc = np.array(graph.transitivity_local_undirected(weights='weight'))
+        nan_mask = np.isnan(cc)
+        pylab.scatter(degree[~nan_mask],cc[~nan_mask],alpha=0.05)
+    # plot empirical
+    graph = co.COB('ROOT').graph(term.flanking_genes(RZM))
+    degree = np.array(graph.degree())
+    cc = np.array(graph.transitivity_local_undirected(weights='weight'))
+    nan_mask = np.isnan(cc)
+    pylab.scatter(degree[~nan_mask],cc[~nan_mask])
+    pylab.xlabel('Local Degree')
+    pylab.ylabel('Clustering Coefficient')
+    if filename is None:
+        filename = "{}_cc.png".format(term.id)
+    pylab.savefig(filename)
+   
