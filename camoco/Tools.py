46fbc673af96f7c7f4ccf921d696f64df8a3ae9f
Rob <schae234@umn.edu>
2014-12-29 13:30:08 -0600

    added a method to clear memoization cache

--- camoco/Tools.py
+++ camoco/Tools.py
@@ -15,13 +15,17 @@ import pandas as pd
 
 def memoize(obj):
     cache = obj.cache = {}
-
     @functools.wraps(obj)
     def memoizer(*args, **kwargs):
+        # Give us a way to clear the cache
+        if 'clear_cache' in kwargs:
+            cache = obj.cache = {}
         # This wraps the calling of the memoized object
         key = str(args) + str(kwargs)
         if key not in cache:
             cache[key] = obj(*args, **kwargs)
+        else:
+            # is in cache, 
         return cache[key]
     return memoizer
 
