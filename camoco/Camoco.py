d5cd270dcec60940984c1df74d337752504a8d4b
Rob <schae234@umn.edu>
2015-04-01 16:58:26 -0500

    now uses classmethod. Makes init method much nices and clear.

--- camoco/Camoco.py
+++ camoco/Camoco.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-from __future__ import print_function,division
+#!/usr/bin/env python3
 import apsw as lite
 import os as os
 import time as time
@@ -8,62 +7,34 @@ import tempfile
 import pandas as pd
 
 from camoco.Tools import log
+from camoco.Config import cf
+from apsw import ConstraintError
 
 class Camoco(object):
-    def __init__(self,name="Camoco",description=None,type='Camoco',basedir="~/.camoco"):
+
+    def __init__(self,name,type='Camoco',basedir="~/.camoco"):
         # Set up our base directory
-        self.basedir = os.path.realpath(os.path.expanduser(basedir))
-        self._create_base_tables()
         self.log = log()
-        if not os.path.exists(self.basedir):
-            # If it doesn't exists, set up first time directories
-            try:    
-                os.mkdir(self.basedir)
-                os.mkdir(os.path.join(self.basedir,"logs"))
-                os.mkdir(os.path.join(self.basedir,"databases"))
-                os.mkdir(os.path.join(self.basedir,"analyses"))
-                os.mkdir(os.path.join(self.basedir,"tmp"))
-            except Exception as e:
-                print("[CAMOCO]",time.ctime(), '-', *args,file=sys.stderr)
-        self.log_file = open(os.path.join(self.basedir,"logs","logfile.txt"),'a')
-        if description is not None:
-            # create new sqlite file for dataset
-            try:
-                self.database('Camoco.Camoco').cursor().execute('''
-                    INSERT OR FAIL INTO datasets (name,description,type)
-                    VALUES (?,?,?)''',(name,description,type))
-            except Exception as e:
-                self.log.warn('CAUTION! {}.{} Database already exists. Dont do anything rash.',name,type)
-        try:
-            # A dataset already exists, return it
-            self.db = self.database(".".join([type,name]))
-            (self.ID,self.name,self.description,
-                self.type,self.added) = self.database('Camoco.Camoco').cursor().execute(
-                "SELECT rowid,* FROM datasets WHERE name = ? AND type = ?",
-                (name,type)
-            ).fetchone()
-            cur = self.db.cursor()  
-            cur.execute('PRAGMA main.page_size = 8192;')
-            cur.execute('PRAGMA main.cache_size= 1000000;')
-            cur.execute('PRAGMA main.temp_store = MEMORY;')
-            #cur.execute('PRAGMA main.locking_mode=EXCLUSIVE;')
-            cur.execute('PRAGMA main.synchronous=OFF;')
-            cur.execute('PRAGMA count_changes=OFF')
-            cur.execute('PRAGMA main.journal_mode=MEMORY;')
-            cur.execute('''
-                CREATE TABLE IF NOT EXISTS globals (
-                    key TEXT,
-                    val TEXT
-                );
-                CREATE UNIQUE INDEX IF NOT EXISTS uniqkey ON globals(key)
-                ''')
-        except Exception as e:
-            raise NameError("Camoco dataset {} does not exist: {}".format(name,e))
+        # A dataset already exists, return it
+        self.db = self._database(".".join([type,name]))
+        (self.ID,self.name,self.description,
+            self.type,self.added) = self._database('Camoco.Camoco').cursor().execute(
+            "SELECT rowid,* FROM datasets WHERE name = ? AND type = ?",
+            (name,type)
+        ).fetchone()
+        cur = self.db.cursor()  
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS globals (
+                key TEXT,
+                val TEXT
+            );
+            CREATE UNIQUE INDEX IF NOT EXISTS uniqkey ON globals(key)
+            ''')
 
     def _resource(self,type,filename):
-        return os.path.expanduser(os.path.join(self.basedir,type,filename))
+        return os.path.expanduser(os.path.join(cf.get('options','basedir'),type,filename))
 
-    def database(self,dbname):
+    def _database(self,dbname):
         # return a connection if exists
         return lite.Connection(self._resource("databases",str(dbname)+".db"))
 
@@ -72,7 +43,7 @@ class Camoco(object):
 
     def _tmpfile(self):
         # returns a handle to a tmp file
-        return tempfile.NamedTemporaryFile(dir=os.path.join(self.basedir,"tmp"))
+        return tempfile.NamedTemporaryFile(dir=os.path.join(cf.get('options','basedir'),"tmp"))
 
     def _global(self,key,val=None):
         # set the global for the dataset
@@ -87,19 +58,49 @@ class Camoco(object):
                 ).fetchone()[0]
             except Exception as e:
                 return None
-            
-    def _create_base_tables(self):
-        camocodb = self.database("camoco")
-        camocodb.cursor().execute(''' 
-            CREATE TABLE IF NOT EXISTS datasets (
-                name TEXT NOT NULL,
-                description TEXT,
-                type TEXT,
-                added datetime DEFAULT CURRENT_TIMESTAMP,
-                PRIMARY KEY(name,type)
-            )
-        ''')
 
     def __getattr__(self,name):
         return self._global(name)
+
+
+    @classmethod
+    def create(cls,name,description,type='Camoco'):
+        '''
+            This is a class method to create a new camoco type object.
+            It initializes base directory hierarchy 
+        '''
+        basedir = os.path.realpath(os.path.expanduser(cf.get('options','basedir')))
+        # Create the basedir if not exists
+
+        try:    
+            os.makedirs(basedir,exist_ok=True)
+            os.makedirs(os.path.join(basedir,"logs"),exist_ok=True)
+            os.makedirs(os.path.join(basedir,"databases"),exist_ok=True)
+            os.makedirs(os.path.join(basedir,"analyses"),exist_ok=True)
+            os.makedirs(os.path.join(basedir,"tmp"),exist_ok=True)
+        except Exception as e:
+            log(' Could not create files in {}',basedir)
+            raise
+        try:
+        # Create the base camoco database
+            lite.Connection(
+                os.path.join(basedir,'databases','Camoco.Camoco.db')
+            ).cursor().execute(''' 
+                CREATE TABLE IF NOT EXISTS datasets (
+                    name TEXT NOT NULL,
+                    description TEXT,
+                    type TEXT,
+                    added datetime DEFAULT CURRENT_TIMESTAMP,
+                    PRIMARY KEY(name,type)
+                );
+                INSERT OR IGNORE INTO datasets (name,description,type)
+                VALUES ('Camoco','Camoco base','Camoco');
+                INSERT OR FAIL INTO datasets (name,description,type)
+                VALUES (?,?,?)''',(name,description,type)
+            )
+        except ConstraintError as e:
+            log.warn('CAUTION! {}.{} Database already exists.',name,type)
+        self = cls(name) 
+        return self
+
 
