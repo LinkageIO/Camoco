70b00d8f6e71e7937007267853d819c887a9035c
Rob <schae234@umn.edu>
2015-04-02 16:06:35 -0500

    Pulled locus code from zfs snapshot, somehow it got bonked

--- camoco/Locus.py
+++ camoco/Locus.py
@@ -3,25 +3,38 @@ from collections import defaultdict
 import re
 
 class Locus(object):
-    def __init__(self, chrom, start, end, id=None ,gene_build=None, organism=None, window=50000):
-        self.id = id
-        self.gene_build = gene_build
-        self.organism = organism
+    def __init__(self, chrom, start, end=None, name=None, window=0, sub_loci=None, **kwargs):
+        self.name = name
         self.chrom = chrom
-        self._start = start
-        self._end = end
-        self.window = window
+        self._start = int(start)
+        self._end = int(end) if end is not None else int(start)
+        self.window = int(window)
+        self.attr = kwargs
+        self.sub_loci = sub_loci if sub_loci is not None else set()
         #  Warn us if something seems off
         if self._start > self._end:
             raise ValueError("Wonky start and stop positions for: {}".format(self))
 
     def as_dict(self):
         return {
-            'id'    : self.id,
+            'name'    : self.name,
             'chrom' : self.chrom,
             'start' : self.start,
             'end'   : self.end
         }
+
+    def as_record(self):
+        return (self.chrom,self.start,self.end,self.name,self.window,self.id)
+
+    @classmethod
+    def from_record(cls,tpl):
+        return cls(*tpl)
+
+    def __setitem__(self,key,val):
+        self.attr[key] = val
+
+    def __getitem__(self,key):
+        return self.attr[key]
 
     @property
     def start(self):
@@ -32,14 +45,9 @@ class Locus(object):
         return int(self._end)
 
     @property
-    def stop(self):
-        ''' because im an idiot ''' 
-        return self.end
-
-    @property
     def coor(self):
         ''' returns a tuple with start and stop '''
-        return (self.start,self.stop)
+        return (self.start,self.end)
 
     @property
     def upstream(self):
@@ -49,27 +57,47 @@ class Locus(object):
     def downstream(self):
         return self.end + self.window
 
+    @property
+    def id(self):
+        return "{}:{}:{}:{}".format(self.name,self.chrom,self.start,self.end)
+
+    def __add__(self,locus):
+        ''' collapse two loci into a new 'meta' locus. The start is the min
+        between the two loci and the window extends within as well as 1/2
+        upstream and downstream of the original window sizes '''
+        # must be on the same chromosome to collapse
+        if self-locus == float('Inf'):
+            raise TypeError('Loci must be on same chromosome to collapse.')
+        new_start = int(max(0,min(self.start,locus.start)))
+        new_end = int(max(self.end,locus.end))
+        new_window = self.window
+        new_id = str(self.id)+'-'+str(locus.id)
+        new_sub_loci = self.sub_loci | locus.sub_loci | set([self.id, locus.id])
+        return Locus(self.chrom,new_start,new_end,window=new_window,sub_loci=new_sub_loci)
+    
     def __eq__(self,locus):
         if (self.chrom == locus.chrom and
             self.start == locus.start and
-            self.end == locus.end and
-            self.gene_build == locus.gene_build and
-            self.organism == locus.organism):
+            self.end == locus.end):
             return True
         else:
             return False
 
     def __contains__(self,locus):
         if (locus.chrom == self.chrom and
-               (( locus.start >= self.start and locus.start <= self.end)
-               or(locus.end   <= self.end   and locus.end   >= self.start)
+               # The locus has as 'start' position within the Locus window
+               (( locus.upstream >= self.upstream and locus.upstream <= self.downstream)
+               # The locus has an 'end' position within the Locus window
+               or(locus.downstream <= self.downstream and locus.downstream >= self.upstream)
             )):
             return True
         else:
             return False
+
+
     def __len__(self):
         ''' inclusive length of locus '''
-        return self.stop - self.start + 1
+        return self.end - self.start + 1
 
     def __cmp__(self,locus):
         if self.chrom == locus.chrom:
@@ -95,179 +123,19 @@ class Locus(object):
             return self.start - other.start
     def __str__(self):
         return '''
-            organism:{} 
-            type: {}
             id: {}
             chromosome: {}
             start: {}
-            end: {}'''.format(self.organism,self.__class__,self.id,self.chrom,self.start,self.stop)
+            end: {}
+            window:{}'''.format(self.id,self.chrom,self.start,self.end,self.window)
     def __repr__(self):
         return str(self)
     def __hash__(self):
         try:
             return int("{}{}".format(self.chrom,self.start))
         except ValueError as e:
-            return int("-1{}".format(abs(self.start)))
-
-class SNP(Locus):
-    def __init__(self, chrom, pos, id=None ,gene_build='5b', organism='Zea',window=50000,sub_snps=None):
-        super().__init__(
-            chrom=str(chrom),
-            start=int(pos)-int(window/2),
-            end=int(pos)+int(window/2),
-            id=id,
-            gene_build=gene_build,
-            organism=organism
-        )
-        self.pos = int(pos)
-        self.window = window
-        self.sub_snps = set([int(pos)])
-        if id is None:
-            self.id = self.summary()
-        if sub_snps is not None:
-            self.sub_snps = self.sub_snps ^ sub_snps
-
-    def __contains__(self,locus):
-        if (locus.chrom == self.chrom and
-               # The locus has as 'start' position within the SNP window
-               (( locus.upstream >= self.upstream and locus.upstream <= self.downstream)
-               # The locus has an 'end' position within the SNP window
-               or(locus.downstream <= self.downstream and locus.downstream >= self.upstream)
-            )):
-            return True
-        else:
-            return False
-
-    def as_dict(self):
-        dict = super().as_dict()
-        dict['start'] = self.start
-        dict['end'] = self.end
-        dict['window'] = self.window
-        dict['pos'] = self.pos
-        return dict
-
-    @property
-    def start(self):
-        # this returns 0 if there are negative positions
-        return max(0,min(self.sub_snps))
-
-    @property
-    def end(self):
-        return max(self.sub_snps)
-
-    def collapse(self,snp):
-        ''' collapse two snps into a new 'meta' SNP. The pos is half way
-        between the two snps and the window extends within as well as 1/2
-        upstream and downstream of the original window sizes '''
-        # must be on the same chromosome to collapse
-        assert(self-snp < float('Inf'))
-        new_pos = int((self.pos + snp.pos)/2)
-        new_window = self.window
-        new_id = str(self.id)+'-'+str(snp.id)
-        new_sub_snps = self.sub_snps ^ snp.sub_snps
-        return SNP(self.chrom,new_pos,window=new_window,sub_snps=new_sub_snps)
-
-    def __str__(self):
-        return '''
-            organism:{} 
-            type: {}
-            id: {}
-            chromosome: {}
-            pos: {}
-            window: {}
-            num_sub_snps: {}'''.format(self.organism,self.__class__,self.id,self.chrom,self.pos,self.window,len(self.sub_snps))
-
-
-    def summary(self):
-        return "S{}:{}".format(self.chrom,','.join([str(x) for x in self.sub_snps]))
-
-    @classmethod
-    def from_str(cls,string,regex='.*(\d+)_(\d+).*'):
-        m = re.search(regex,string)
-        if m is not None:
-            return cls(m.group(1),m.group(2))
-       
+            return int("-{}".format(abs(self.start)))
 
 class Gene(Locus):
-    def __init__(self,chrom=None,start=None,end=None,strand=None,id=None,build='5b',organism='Zea'):
-        super().__init__(chrom,start,end,id,build,organism) 
-        self.strand = strand
-        self.attributes = dict()
-    def __str__(self):
-        return self.id
-    def __repr__(self):
-        return str(self)
-    def summary(self):
-        return "Gene-{}-{}:{}-{}".format(self.id,self.chrom,self.start,self.end)
-
-    def to_gtf(self):
-        return "\t".join(
-            [self.chrom,'Camoco','Gene',self.start,self.end,".",self.strand,'0',
-            "ID={};Organism={};Build={}".format(self.id, self.organism, self.build)]
-        )
-
-    @classmethod
-    def from_gtf(cls,line,attribute_id="ID"):
-        # GTF fields MUST be tab seperated as per Ensemble rules
-        seqname,source,feature,start,end,score,strand,frame,attributes = line.strip().split()
-        attributes = attributes.replace('=',';').split(";")
-        attributes = dict(zip(attributes[::2],attributes[1::2]))
-        if 'build 'not in attributes:
-            attributes['build'] = None
-        if 'organism' not in attributes:
-            attributes['organism'] = None
-        self = cls(chrom=seqname,start=start,end=end,strand=strand,id=attributes[attribute_id],build=attributes['build'],organism=attributes['organism'])
-        self.attributes = attributes
-        return self
-
-class QTL(Locus):
-    def __init__(self,chrom,start,end,id=None,build='5b',organism='Zea'):
-        if id == None:
-            self.id = "QTL-chr{}:{}-{}".format(chrom,start,end)
-        else:
-            self.id = id
-        super().__init__(str(chrom),start,end,self.id)
-    def __str__(self):
-        return "{}".format(self.id)
-
-
-# -----------------------End of Class Definitions -------------------------------------------
-
-# Useful abstractions for analyses
-def bootstrap(loci,genome):
-    ''' Returns a non-overlapping, randoms set of equal 
-        size loci from the genome '''
-    random_loci = [genome.rLocus(len(locus)) for locus in loci]
-    if non_overlapping(random_loci):
-        return random_loci
-    else:
-        return bootstrap(loci,genome)
-
-def num_overlap(loci1,loci2,unique=False):
-    ''' Returns the number of loci in list 1 which overlap list 2 
-        If unique is True, function return the number of unique loci 
-        overlapping between lists '''
-    total = defaultdict(int)
-    for x in loci1:
-        for y in loci2:
-            if x in y:
-                total[str(y)] += 1
-    if unique:
-        return len(total)
-    else:
-        return sum(total.values())
-
-def non_overlapping(loci_list):
-    ''' Return True if loci do not overlap with one another
-        otherwise returns False if any two loci overlap '''
-    if num_overlap(loci_list,loci_list) > len(loci_list):
-        return False
-    else:
-        return True    
-
-def significant_overlap(loci1,loci2,genome,iterations=1000,unique=False):
-    ''''Returns the number of bootstrapped overlaps larger than or equal to loci1 in loci2
-        for i iterations '''
-    emp_overlap = num_overlap(loci1,loci2,unique=unique)
-    bs = [ num_overlap(loci1,bootstrap(loci2,genome),unique=unique) for x in range(iterations)]
-    return sum([x >= emp_overlap for x in bs])/iterations
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
