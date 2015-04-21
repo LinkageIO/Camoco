66b1c0c87da5e005da262f7fbd1013b30fe7e256
Rob <schae234@umn.edu>
2015-04-21 09:31:10 -0500

    added test to ensure that neighbors returns as many edges as global_degree in COB

--- tests/BaseTest.py
+++ tests/BaseTest.py
@@ -9,15 +9,11 @@ import pandas as pd
 import itertools
 from camoco.Config import cf
 
-# Set the basedir to the testdir
-cf.set('options','basedir', cf.get('options','testdir'))
-
 class LocusBase(unittest.TestCase):
     def test_locus_initialization(self):
         # numeric chromosomes
         a = co.Locus(1,500)
         self.assertIsInstance(a,co.Locus)
-
 
 class BaseRefGen(unittest.TestCase):
     @unittest.skipUnless(
@@ -32,23 +28,31 @@ class BaseRefGen(unittest.TestCase):
             'Bootstrap vs emirical candidate gene sizes do not match'
         )
 
+    @unittest.skipUnless(co.available_datasets('RefGen','Zm5bFGS'),'Not Adequate Datasets')
+    def test_get_attr_and_generating_from_ids(self):
+        ZM = co.RefGen('Zm5bFGS')
+        self.assertIsInstance(ZM['GRMZM2G000014'],co.Locus)
+        self.assertIsInstance(ZM.from_ids(['GRMZM2G000014'])[0],co.Locus)
+
 class BaseCOB(unittest.TestCase):
 
     @unittest.skipUnless(co.available_datasets('Expr','ZmRoot'),'ZmRoot not defined')
     def setUp(self):
         self.cob = co.COB('ZmRoot')
 
-    @unittest.skipUnless(co.available_datasets('Expr','ZmRoot'),'ZmRoot not defined')
     def ZmRoot(self):
         self.assertEqual(len(self.cob.coex), comb(self.cob.num_genes(),2),
                 "coex dimentions mismatch")
 
-    @unittest.skipUnless(co.available_datasets('Expr','ZmRoot'),'ZmRoot not defined')
     def test_coexpress_concordance(self):
         for a,b in itertools.combinations([self.cob.refgen.random_gene() for x in range(50)],2):
             self.assertTrue(abs(self.cob.coexpression(a,b).score - self.cob._coex_concordance(a,b)) < 0.001)
             dis_dif = abs(self.cob.coexpression(a,b).distance - abs(a-b))
             self.assertTrue( np.isnan(dis_dif) or dis_dif < 0.001)
+
+    def test_num_neighbors_equals_degree(self):
+        random_gene = self.cob.refgen.random_gene()
+        self.assertTrue(len(self.cob.neighbors(random_gene) == self.cob.global_degree(random_gene)))
 
 if __name__ == '__main__':
     unittest.main()
