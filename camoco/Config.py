988831aa77bc655b36c7d6707d55a8bbbda2250a
Rob <schae234@umn.edu>
2015-07-07 12:44:39 -0500

    Now if config file is absent, it creates one

--- camoco/Config.py
+++ camoco/Config.py
@@ -3,8 +3,31 @@
 import os
 import configparser
 
+
+
 global cf
 cf = configparser.ConfigParser()
 cf._interpolation = configparser.ExtendedInterpolation()
+
+cf_file = os.path.expanduser('~/.camoco.conf')
+
+default_config = '''
+[options]
+basedir = /heap/camoco
+testdir = /heap/camoco
+
+[test]
+refgen   = Zm5bFGS
+cob      = NewRoot
+ontology = ZmIonome 
+term     = Fe57
+gene     = GRMZM2G000014
+'''
+
+# Check to see if 
+if not os.path.isfile(cf_file):
+    with open(cf_file,'w') as CF:
+        print(default_config,file=CF)
+
 cf.read(os.path.expanduser('~/.camoco.conf'))
 
