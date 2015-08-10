ca48f3851d5958513f0fa63bcfc9507dbd20d51d
Rob S <schae234@gmail.com>
2015-08-10 13:18:39 -0500

    Fix BaseTests on clean build, should run on Travis.

--- camoco/__init__.py
+++ camoco/__init__.py
@@ -1,17 +1,18 @@
-""" 
+"""
 
-Camoco Library - CoAnalysis of Molecular Components 
+Camoco Library - CoAnalysis of Molecular Components
 
 CacheMoneyCorn
 
 """
 
-__license__ = """ 
+__license__ = """
     Creative Commons Non-Commercial 4.0 Generic
     http://creativecommons.org/licenses/by-nc/4.0/
 """
 
-import pyximport; pyximport.install() 
+import pyximport
+pyximport.install()
 
 from camoco.Camoco import Camoco
 from camoco.Expr import Expr
@@ -24,3 +25,5 @@ from camoco.Tools import available_datasets,del_dataset,\
                          mv_dataset,redescribe_dataset
 from camoco.Config import cf
 from camoco.GEO import Family
+
+Camoco.create('Camoco','Recursion.')
