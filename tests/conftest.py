a4772d82ae5064613b1b6888d65a9602c9a38dfd
Rob <schae234@umn.edu>
2015-08-20 00:15:40 -0500

    Transfer over build tests from unittest framework to pytest framework.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from camoco.Config import cf
 
 import camoco as co
 
-@pytest.fixture()
+@pytest.fixture
 def Zm5bFGS():
     if cf['test'].getboolean('force'):
         co.del_dataset('RefGen','Zm5bFGS',safe=False)
@@ -15,12 +15,14 @@ def Zm5bFGS():
             cf['options']['testdir'],
             'raw','RefGen','ZmB73_5b_FGS.gff.gz'
         )
-        co.RefGen.from_gff(
+        # This is stupid and necessary because pytables wont let me open
+        # more than one table
+        return co.RefGen.from_gff(
             gff,'Zm5bFGS','Maize 5b Filtered Gene Set','5b','Zea Mays'
         )
     return co.RefGen('Zm5bFGS')
 
-@pytest.fixture()
+@pytest.fixture
 def AtTair10():
     if cf['test'].getboolean('force'):
         co.del_dataset('RefGen','AtTair10',safe=False)
@@ -31,20 +33,20 @@ def AtTair10():
                 'raw','RefGen','TAIR10_GFF3_genes.gff.gz'
             )
         )
-        co.RefGen.from_gff(
+        return co.RefGen.from_gff(
             gff,'AtTair10','Tair 10','10','Arabidopsis'
         )
-    return co.RefGen('AtTair10')
+    else:
+        return co.RefGen('AtTair10')
 
-
-@pytest.fixture()
+@pytest.fixture
 def ZmRNASeqTissueAtlas(Zm5bFGS):
     if cf['test'].getboolean('force'):
         co.del_dataset('COB','ZmRNASeqTissueAtlas',safe=False)
         co.del_dataset('Expr','ZmRNASeqTissueAtlas',safe=False)
     if not co.available_datasets('COB','ZmRNASeqTissueAtlas'):
         # Build it 
-        ZmRoot = co.COB.from_table(
+        return co.COB.from_table(
             os.path.join(cf.get('options','testdir'),
                 'raw', 'Expr', 'MaizeRNASeqTissue.tsv.gz',
             ),
@@ -59,9 +61,10 @@ def ZmRNASeqTissueAtlas(Zm5bFGS):
             quantile=False,
             max_val=300
         )
-    return co.COB('ZmTissueAtlas')
+    else:
+        return co.COB('ZmTissueAtlas')
 
-@pytest.fixture()
+@pytest.fixture
 def AtSeed(AtTair10):
     if cf['test'].getboolean('force'):
         co.del_dataset('Expr','AtSeed',safe=False)
@@ -78,7 +81,7 @@ def AtSeed(AtTair10):
             for x in Seed ]
         )
         #SeedFam.to_keepfile("SeedKeep.tsv",keep_hint='seed')
-        AtSeed = co.COB.from_DataFrame(
+        return co.COB.from_DataFrame(
             SeedFam.series_matrix(
                 keepfile=os.path.join(
                     cf.get('options','testdir'),
@@ -86,10 +89,119 @@ def AtSeed(AtTair10):
                 )
             ),
             'AtSeed','Arabidopsis Seed',
-            co.RefGen('T10'),rawtype='MICROARRAY',
+            AtTair10,
+            rawtype='MICROARRAY',
             quantile=True
     
         )
-    return AtSeed
+    else:
+        return co.COB('AtSeed')
+
+
+@pytest.fixture
+def AtGen(AtTair10):
+    if cf['test'].getboolean('force'):
+        co.del_dataset('Expr','AtGen',safe=False)
+    if not co.available_datasets('COB','AtGen'):
+        General = ['GSE18975','GSE39384','GSE19271','GSE5632','GSE39385',
+                'GSE5630','GSE15617','GSE5617','GSE5686','GSE2473',
+                'GSE5633','GSE5620','GSE5628','GSE5624',
+                'GSE5626','GSE5621','GSE5622','GSE5623','GSE5625','GSE5688']
+        GenFam = sum(
+            [co.Family.from_file(
+                os.path.join(
+                    cf.get('options','testdir'),
+                    'raw','GSE','{}_family.soft'.format(x)
+                )
+            )
+            for x in General ]
+        )
+        #GenFam.to_keepfile("GenKeep.tsv")
+        return co.COB.from_DataFrame(
+            GenFam.series_matrix(
+                keepfile=os.path.join(
+                    cf.get('options','testdir'),
+                    'raw','GSE','GenKeep.tsv'
+                )
+            ),
+            'AtGen','Arab General',
+            AtTair10,
+            rawtype='MICROARRAY',
+            quantile=True
+        )
+    else:
+        return co.COB('AtGen')
+
+@pytest.fixture
+def AtLeaf(AtTair10):
+    if cf['test'].getboolean('force'):
+        co.del_dataset('Expr','AtLeaf',safe=False)
+    if not co.available_datasets('COB','AtLeaf'):
+        Leaf = ['GSE14578','GSE5630','GSE13739', #'GSE26199',
+                'GSE5686','GSE5615','GSE5620','GSE5628',
+                'GSE5624','GSE5626','GSE5621','GSE5622',
+                'GSE5623','GSE5625','GSE5688']
+        LeafFam = sum(
+            [co.Family.from_file(
+                os.path.join(
+                    cf.get('options','testdir'),
+                    'raw','GSE','{}_family.soft'.format(x)
+                )
+            )
+            for x in Leaf ]
+        )
+        #LeafFam.to_keepfile("LeafKeep.tsv",keep_hint="lea")
+        return co.COB.from_DataFrame(
+            LeafFam.series_matrix(
+                keepfile=os.path.join(
+                    cf.get('options','testdir'),
+                    'raw','GSE','LeafKeep.tsv'
+                )
+            ),
+            'AtLeaf','Arabidopsis Leaf',
+            AtTair10,
+            rawtype='MICROARRAY',
+            max_gene_missing_data=0.3,
+            min_expr=0.01,
+            quantile=True,
+        )
+    else:
+        return co.COB('AtLeaf')
+
+@pytest.fixture
+def AtRoot(AtTair10):
+    if cf['test'].getboolean('force'):
+        co.del_dataset('Expr','AtRoot',safe=False)
+    if not co.available_datasets('COB','AtRoot'):
+        Root = ['GSE14578','GSE46205','GSE7631','GSE10576','GSE42007',
+                'GSE34130','GSE21611','GSE22966','GSE7641','GSE5620',
+                'GSE8934','GSE5628','GSE30095','GSE30097','GSE5624',
+                'GSE5626','GSE5749','GSE5621','GSE5622',
+                'GSE5623','GSE5625','GSE5688']
+        RootFam = sum(
+            [co.Family.from_file(
+                os.path.join(
+                    cf['options']['testdir'],
+                    'raw','GSE','{}_family.soft'.format(x)
+                )
+            )
+            for x in Root ]
+        )
+        #RootFam.to_keepfile("RootKeep.tsv",keep_hint='root')
+        return co.COB.from_DataFrame(
+            RootFam.series_matrix(
+                keepfile=os.path.join(
+                    cf.get('options','testdir'),
+                    'raw','GSE','RootKeep.tsv')
+            ),
+            'AtRoot','Arab Root',
+            AtTair10,
+            rawtype='MICROARRAY',
+            quantile=True
+        )
+    else:
+        return co.COB('AtRoot')
+
+
 
 
--- tests/test_COB.py
+++ tests/test_COB.py
@@ -10,3 +10,12 @@ def test_init(ZmRNASeqTissueAtlas):
 
 def test_AtSeed(AtSeed):
     assert AtSeed
+
+def test_AtGen(AtGen):
+    assert AtGen
+
+def test_AtRoot(AtRoot):
+    assert AtRoot
+
+def test_AtLeaf(AtLeaf):
+    assert AtLeaf
