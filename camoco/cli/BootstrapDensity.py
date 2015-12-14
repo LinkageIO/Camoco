7786ca1eeefaaa9ae87f4cf9f2a06d6eafa2f2e8
Rob <schae234@umn.edu>
2015-12-14 07:57:23 -0600

    Reflow code.

--- camoco/cli/BootstrapDensity.py
+++ camoco/cli/BootstrapDensity.py
@@ -27,12 +27,11 @@ def density(args):
         os.makedirs(os.path.dirname(args.out),exist_ok=True)
     if os.path.exists(args.out):
         print(
-            "{} exists! Skipping!".format(
+            "Output for {} exists! Skipping!".format(
                 args.out
-            )
+            ),file=sys.stderr
         )
         return
-
 
     if 'all' in args.terms:
         terms = ont.iter_terms()
@@ -61,82 +60,48 @@ def density(args):
     
         if 'effective' in args.snp2gene:
             # Map to effective
-            effective_loci = term.effective_loci(
+            loci = term.effective_loci(
                 window_size=args.candidate_window_size
             )
-    
-            # Grab those candidates
-            effective_candidates = cob.refgen.candidate_genes(
-                effective_loci, flank_limit=args.candidate_flank_limit
-            )
-            effective_emp = cob.trans_locus_density(
-                effective_loci,
-                flank_limit=args.candidate_flank_limit,
-                bootstrap=False
-            )
-
-            # ----------------------------
-            # Effective Loci Bootstraps
-            effective_bootstraps = np.array([
-                cob.trans_locus_density(
-                    effective_loci,
-                    flank_limit=args.candidate_flank_limit,
-                    bootstrap=True
-                ) for x in range(args.num_bootstraps)
-            ])
-            
-            effective_pval = sum([bs >= effective_emp for bs in effective_bootstraps])
-            if effective_pval > 0:
-                effective_pval = effective_pval / args.num_bootstraps
-        
-
-       
-            results['NumCollapsedSNPs'] = len(effective_loci)
-            results['NumEffectiveCandidates'] = len(effective_candidates)
-            results['EffectiveDensity'] = effective_emp
-            results['EffectivePValue'] = effective_pval
-            results['EffectiveBSMean'] = effective_bootstraps.mean()
-            results['EffectiveBSStd'] = effective_bootstraps.std()
-            
-       
-        if 'strongest' in args.snp2gene:
-            strongest_loci = term.strongest_loci(
+        elif 'strongest' in args.snp2gene:
+            loci = term.strongest_loci(
                 window_size=args.candidate_window_size,
                 attr=args.strongest_attr,
                 lowest=args.strongest_higher
             )
-            strongest_candidates = cob.refgen.candidate_genes(
-                strongest_loci, flank_limit=args.candidate_flank_limit
-            )
-            strongest_emp = cob.trans_locus_density(
-                strongest_loci,
+     
+        # Grab those candidates
+        candidates = cob.refgen.candidate_genes(
+            loci, flank_limit=args.candidate_flank_limit
+        )
+        emp = cob.trans_locus_density(
+            loci,
+            flank_limit=args.candidate_flank_limit,
+            bootstrap=False
+        )
+
+        # ----------------------------
+        # Effective Loci Bootstraps
+        bootstraps = np.array([
+            cob.trans_locus_density(
+                loci,
                 flank_limit=args.candidate_flank_limit,
-                bootstrap=False
-            )
-            results['NumStrongestCandidates'] = len(strongest_candidates)
-            results['StrongestDensity'] = strongest_emp
-            # ----------------------------
-            # strongest Loci Bootstraps
-            strongest_bootstraps = np.array([
-                cob.trans_locus_density(
-                    strongest_loci,
-                    flank_limit=args.candidate_flank_limit,
-                    bootstrap=True
-                ) for x in range(args.num_bootstraps)
-            ])
+                bootstrap=True
+            ) for x in range(args.num_bootstraps)
+        ])
+        
+        effective_pval = sum([bs >= effective_emp for bs in bootstraps])
+        if effective_pval > 0:
+            effective_pval = effective_pval / args.num_bootstraps
+        
+        results['NumCollapsedSNPs'] = len(loci)
+        results['NumCandidates'] = len(candidates)
+        results['Density'] = emp
+        results['PValue'] = effective_pval
+        results['BSMean'] = effective_bootstraps.mean()
+        results['BSStd'] = effective_bootstraps.std()
+        results['SNP2GeneMethod'] = args.snp2gene
             
-            strongest_pval = sum([bs >= strongest_emp for bs in strongest_bootstraps])
-            if strongest_pval > 0:
-                strongest_pval = strongest_pval / args.num_bootstraps
-        
-            results['StrongestPValue'] = strongest_pval
-        
-            results['NumCollapsedSNPs'] = len(strongest_loci)
-            results['NumStrongestCandidates'] = len(strongest_candidates)
-            results['StrongestDensity'] = strongest_emp
-            results['StrongestPValue'] = strongest_pval
-            results['StrongestBSMean'] = strongest_bootstraps.mean()
-            results['StrongestBSStd'] = strongest_bootstraps.std()
         all_results.append(results)
     
     # Return the results 
