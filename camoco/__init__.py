da4afd67dc11cf6c2cfa2079284240849c361af3
Rob S <schae234@gmail.com>
2016-04-17 10:26:31 -0500

    get rid of pyxinstall and rely on setup.py to do the right thing

--- camoco/__init__.py
+++ camoco/__init__.py
@@ -11,14 +11,20 @@ __license__ = """
     http://creativecommons.org/licenses/by-nc/4.0/
 """
 
-import pyximport
-pyximport.install()
+#import pyximport
+#pyximport.install()
+
+import sys
+import os
+sys.path.insert(0,os.path.expanduser('~/.camoco/.conda/camoco/lib/python3.4/site-packages'))
 
 from .Config import cf
 from .Camoco import Camoco
 from .Expr import Expr
 from .COB import COB
 from .RefGen import RefGen
+from .RefGenDist import *
+from .PCCUP import *
 from .Ontology import Ontology,Term
 from .GWAS import GWAS
 from .HapMap import HapMap
