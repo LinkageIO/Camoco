c88b372d72b0b61f12d65c4c5ee5d70d63c9cf8e
Rob <schae234@umn.edu>
2016-07-20 15:53:28 -0500

    overhail simulateGWAS API, add support for missing-SNPs

--- camoco/cli/commands/simulateGWAS.py
+++ camoco/cli/commands/simulateGWAS.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import math
 
 import pandas as pd
 import numpy as np
@@ -8,59 +9,79 @@ from collections import OrderedDict
 
 def simulateGWAS(args):
     # Make sure that output files are handled correctly 
+    import ipdb; ipdb.set_trace()
     if args.out != sys.stdout:
         args.out = "{}_GWASSim.csv".format(args.out.replace('.csv',''))        
-    if os.path.dirname(args.out) != '':
-        os.makedirs(os.path.dirname(args.out),exist_ok=True)
-    if os.path.exists("{}_GWASSim.csv".format(args.out.replace('.csv',''))):
-        print(
-            "{}_GWASSim.csv exists! Skipping!".format(
-                args.out.replace('.csv','')
+        if os.path.dirname(args.out) != '':
+            os.makedirs(os.path.dirname(args.out),exist_ok=True)
+        if os.path.exists("{}_GWASSim.csv".format(args.out.replace('.csv',''))):
+            print(
+                "{}_GWASSim.csv exists! Skipping!".format(
+                    args.out.replace('.csv','')
+                )
             )
-        )
-        return None
-
+            return None
+    # Load camoco objects
     go = co.GOnt(args.GOnt)
     cob = co.COB(args.cob)
     if 'all' in args.terms:
         terms = go.iter_terms()
+    elif os.path.exists(args.terms[0]):
+        terms = [go[x.strip()] for x in open(args.terms[0]).readlines()]
     else:
         # Otherwise get the term out of the GWAS
         terms = [ go[x] for x in args.terms ] 
-        terms = filter(
-            lambda x: len(x.loci) >= args.min_term_size or len(x.loci) <= args.max_term_size,
-            terms
-        )
+    terms = [ x for x in terms if len(x.loci) >= args.min_term_size and len(x.loci) <= args.max_term_size ]
+    cob.log("Simulating GWAS for {} GO Terms",len(terms))
+    cob.log("All terms are between {} and {} 'SNPs'", args.min_term_size, args.max_term_size)
 
     results = []
     for i,term in enumerate(terms):
-        cob.log('On term {}',i)   
         window_size = args.candidate_window_size
         flank_limit =  args.candidate_flank_limit
         # Generate a series of densities for parameters
         num_genes = len(term.loci)
         if num_genes == 0:
             continue
-        eloci = term.effective_loci(
+        eloci = [ x for x  in term.effective_loci(
             window_size=window_size
-        )
+        ) if x in cob ]
+        cob.log('-'*75)
+        cob.log(
+            'Simulation {}: {} ({}/{} genes in {})',
+            i,term.id,len(eloci),num_genes,cob.name
+        )   
+        # Remove a percentage of SNPs to simulate false negatives
+        if args.percent_SNPs_missed != None and args.percent_SNPs_missed > 0:
+            # Calulate the index needed to hit percent missing
+            missing_index = math.ceil(len(eloci) * (1-(args.percent_SNPs_missed/100)))
+            new_eloci = np.random.permutation(eloci)[0:missing_index]
+            cob.log('Simulating {}% of SNPs missed by GWAS ({} genes -> {})',args.percent_SNPs_missed,len(eloci),len(new_eloci))
+            eloci = new_eloci
+        # Replace a percentage of SNPs with false positives
         if args.percent_fcr != None and args.percent_fcr > 0:                                            
             # replace some loci with random genes if FDR specified              
-            num_fcr = int(len(eloci) * args.percent_fcr)                        
+            num_fcr = math.ceil(len(eloci) * (args.percent_fcr/101))               
             fcr_loci = cob.refgen.random_genes(num_fcr,window=window_size) 
+            cob.log('Simulating {}% of SNPs as false positive -> adding {} SNPs',args.percent_fcr,len(fcr_loci))
             # permute and truncate the loci then add fcr loci                   
             eloci = np.concatenate([                                            
-                np.random.permutation(eloci)[0:-1*len(fcr_loci)],                   
+                #np.random.permutation(eloci)[0:-1*len(fcr_loci)],                   
+                eloci,
                 np.array(list(fcr_loci))
             ])   
         candidates = cob.refgen.candidate_genes(
             eloci,
             flank_limit=flank_limit
         )
+        cob.log("SNP to gene mapping finds {} genes at window:{} bp, flanking:{} genes",len(candidates),args.candidate_window_size,args.candidate_flank_limit)
         if len(candidates) == 0:
             density = np.NaN
             trans_density = np.NaN
-            pval = np.NaN
+            density_pval = np.NaN
+            locality = np.NaN
+            trans_locality = np.NaN
+            locality_pval = np.NaN
         else:
             # Density
             density = cob.density(
@@ -101,18 +122,19 @@ def simulateGWAS(args):
         results.append(OrderedDict([
            ('COB',cob.name),
            ('GO',term.id),
-           ('NumGenes',num_genes),
+           ('NumRealGenes',num_genes),
            ('WindowSize',window_size),
            ('FlankLimit',flank_limit),
            ('FCR',args.percent_fcr),
+           ('MissedSNPsRate',args.percent_SNPs_missed),
            ('NumEffective',len(eloci)),
            ('NumCandidates',len(candidates)),
            ('Density',density),
            ('TransDensity',trans_density),
-           ('Density_Pval',density_pval),
+           ('Density_pval',density_pval),
            ('Locality',locality),
            ('TransLocality',trans_locality),
-           ('Locality_Pval',locality_pval)
+           ('Locality_pval',locality_pval)
         ]))
     results = pd.DataFrame.from_dict(results)
     results.to_csv(args.out,sep='\t',index=False)
