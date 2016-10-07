a0d43386f17b10405464127d33b3e0ef86d7e637
Rob Schaefer <schae234@gmail.com>
2016-10-07 12:48:32 -0500

    OverlapAnalysis is now a class

--- camoco/cli/commands/OverlapAnalysis.py
+++ camoco/cli/commands/OverlapAnalysis.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+import matplotlib
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import matplotlib.pylab as plt
+matplotlib.style.use('ggplot')
+matplotlib.use('Agg')
+
+
 import argparse
 import sys
 import os
@@ -13,15 +21,26 @@ from collections import OrderedDict
 
 import camoco as co
 import pandas as pd
-import matplotlib.pylab as plt
+pd.set_option('display.width',300)
+import glob as glob
+
+
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),    
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),    
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),    
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),    
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]    
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.    
+for i in range(len(tableau20)):    
+    r, g, b = tableau20[i]    
+    tableau20[i] = (r / 255., g / 255., b / 255.)
+ 
 
 class OverlapAnalysis(object):
+    def __init__(self):
+        self.results = pd.DataFrame()
 
-    def __init__(self,args):
-        # Build base camoco objects
-        self.args = args
-        self.cob = co.COB(args.cob)
-        self.ont = co.GWAS(args.gwas)
+    def generate_output_name(self):
         # Handle the different output schemes
         if self.args.out is None:
             self.args.out = '{}_{}_{}_{}_{}'.format(
@@ -31,17 +50,16 @@ class OverlapAnalysis(object):
                 self.args.candidate_flank_limit,
                 ':'.join(args.terms)
             )
-        self.args.out = os.path.splitext(args.out)[0] + '.{}.overlap.tsv'.format(self.args.method)
-        if os.path.dirname(args.out) != '':
-            os.makedirs(os.path.dirname(args.out),exist_ok=True)
-        if os.path.exists(args.out) and self.args.force != True:
+        self.args.out = os.path.splitext(self.args.out)[0] + '.{}.overlap.tsv'.format(self.args.method)
+        if os.path.dirname(self.args.out) != '':
+            os.makedirs(os.path.dirname(self.args.out),exist_ok=True)
+        if os.path.exists(self.args.out) and self.args.force != True:
             print(
                 "Output for {} exists! Skipping!".format(
                     self.args.out
                 ),file=sys.stderr
             )
             return
-        self.analyze()
 
     def snp2gene(self,term):
         if 'effective' in self.args.snp2gene:
@@ -56,7 +74,27 @@ class OverlapAnalysis(object):
                 lowest=self.args.strongest_higher
             )
 
-    def analyze(self):
+    @classmethod
+    def from_csv(cls,dir='./',sep='\t'):
+        self = cls()
+        # Read in Gene specific data
+        self.results = pd.concat(
+            [pd.read_table(x,sep=sep) \
+                for x in glob.glob(dir+"/*.overlap.tsv") ]
+        )
+        return self
+
+    @classmethod
+    def from_CLI(cls,args):
+        '''
+            Implements an interface for the CLI to perform overlap analysis
+        '''
+        self = cls()
+        # Build base camoco objects
+        self.args = args
+        self.cob = co.COB(args.cob)
+        self.ont = co.GWAS(args.gwas)
+
         # Generate a terms iterable
         if 'all' in self.args.terms:
             terms = self.ont.iter_terms()
@@ -119,9 +157,10 @@ class OverlapAnalysis(object):
             overlap['TermCollapsedLoci'] = len(loci)
             overlap['TermPValue'] = overlap_pval
             overlap['NumBootstraps'] = self.args.num_bootstraps
+            overlap['Method'] = self.args.method
             results.append(overlap.reset_index())
-        results = pd.concat(results)
-        results.to_csv(self.args.out,sep='\t',index=None)
+        self.results = pd.concat(results)
+        self.results.to_csv(self.args.out,sep='\t',index=None)
 
     def overlap(self,loci,bootstrap=False,iter_name=None):
         '''
@@ -148,3 +187,57 @@ class OverlapAnalysis(object):
                 include_regression=True,
             ).rename(columns={'resid':'score'})
             
+    def plot_pval_heatmap(self,filename=None,pval_cutoff=0.05):
+        '''
+            Generate a heatmap based on TermPVal
+        '''
+        cobs = self.results.COB.unique()
+        fig,axes = plt.subplots(len(cobs),1,sharex=True)
+        fig.set_size_inches(8,11)
+        for i,cob in enumerate(cobs):
+            data = pd.pivot_table(
+                self.results.ix[self.results.COB==cob],
+                index=['WindowSize','FlankLimit'],
+                columns=['Term'],
+                values='TermPValue',
+                aggfunc=np.mean
+            )
+            #data[data > pval_cutoff] = np.nan
+            #data[data < pval_cutoff] = 0
+            axes[i].set_frame_on(False)
+            cmap = plt.cm.Greens_r
+            cmap.set_bad('white',1.0)
+            im = axes[i].pcolormesh(
+                np.ma.masked_invalid(data),
+                cmap=cmap,
+                #linewidth=0.1,
+                edgecolor='lightgrey',
+                vmin=0,
+                vmax=0.05
+            )
+            # Make the layout more natural
+            axes[i].set_ylabel(cob,fontsize=20)
+            axes[i].yaxis.set_label_position("right")
+            axes[i].invert_yaxis()
+            axes[i].set(
+                yticks=np.arange(len(data.index))+0.5
+            )
+            axes[i].yaxis.set_ticks_position('left')
+            axes[i].xaxis.set_ticks_position('none')
+            if i == 0:
+                axes[i].xaxis.tick_top()
+                axes[i].set_xticks(np.arange(len(data.columns))+0.5)
+                axes[i].set_xticklabels(data.columns.values, rotation=90)
+            axes[i].set_yticklabels(data.index.values)
+        # Create a new axis to append the color bar to
+        divider = make_axes_locatable(axes[len(cobs)-1])
+        cax = divider.append_axes("bottom", size="5%", pad=0.05)
+        cbar = fig.colorbar(
+            im, cax=cax, orientation='horizontal', ticks=[0,0.025,0.05]
+        )
+        cbar.set_ticklabels(['0','0.025','≥0.05'])
+        plt.tight_layout(pad=0.4,w_pad=0.5, h_pad=1.0)
+        if filename is not None:
+            plt.savefig(filename,dpi=300)
+            plt.close()
+        return (fig,axes)
