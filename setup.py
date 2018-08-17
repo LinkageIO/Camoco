002ddfdef9f111b263446d9d48f25bf16607d12b
rob <rob@linkage.io>
2018-08-17 15:23:10 -0600

    enforce python 3.6 or above

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
     package_data = {
         '':['*.cyx']    
     },
+    python_requires='>=3.6',
     setup_requires = [
         # Setuptools 18.0 properly handles Cython extensions.
         'setuptools>=18.0',
