2a86680dbf09793fe9ca4ad7d03e714cf507e1a3
Rob Schaefer <rob@linkage.io>
2018-08-17 09:47:04 -0600

    add a file rollback when build-cob errors

--- camoco/cli/commands/build.py
+++ camoco/cli/commands/build.py
@@ -4,57 +4,62 @@ from camoco.Tools import DummyRefGen,log,available_datasets
 from camoco.Locus import Gene
 
 def build_cob(args):
-    # Build the refgen
-    refgen = co.RefGen(args.refgen)
-    # Check that the sep is likely right.
-    if len(pd.read_table(args.filename,sep=args.sep).columns) == 1:
-        print(
-            ("Detected only 1 column in {}, are you sure "
-            "colunms are separated by '{}'?").format(args.filename,args.sep)
-        )
-        return None
-    if args.allow_non_membership:
-        refgen = refgen.copy(
-            '{}_tmp'.format(refgen.name), 
-            'temp refgen'.format(refgen.name)
-        )
-        # Add non membership genes
-        for gid in pd.read_table(args.filename,sep=args.sep).index:
-            refgen.add_gene(Gene(None,None,id=gid))
+    try:
+        # Build the refgen
+        refgen = co.RefGen(args.refgen)
+        # Check that the sep is likely right.
+        if len(pd.read_table(args.filename,sep=args.sep).columns) == 1:
+            print(
+                ("Detected only 1 column in {}, are you sure "
+                "colunms are separated by '{}'?").format(args.filename,args.sep)
+            )
+            return None
+        if args.allow_non_membership:
+            refgen = refgen.copy(
+                '{}_tmp'.format(refgen.name), 
+                'temp refgen'.format(refgen.name)
+            )
+            # Add non membership genes
+            for gid in pd.read_table(args.filename,sep=args.sep).index:
+                refgen.add_gene(Gene(None,None,id=gid))
 
-    quality_control = False if args.skip_quality_control else True
-    normalize = False if args.skip_normalization else True
+        quality_control = False if args.skip_quality_control else True
+        normalize = False if args.skip_normalization else True
 
-    # Check to see if this dataset is already built
-    if available_datasets('Expr',args.name):
-        print('Warning! This dataset has already been built.')
-        co.Tools.del_dataset('Expr',args.name,force=args.force)
-        
-    # Basically just pass all the CLI arguments to the COB class method  
-    cob = co.COB.from_table(
-        args.filename,
-        args.name,
-        args.description,
-        refgen,
-        # Optional arguments
-        sep=args.sep,
-        rawtype=args.rawtype,
-        # Data Processing
-        quality_control=quality_control,
-        normalization=normalize,
-        quantile=args.quantile,
-        # Data processing parameters
-        max_gene_missing_data=args.max_gene_missing_data,
-        max_accession_missing_data=args.max_accession_missing_data,
-        min_single_sample_expr=args.min_single_sample_expr,
-        min_expr=args.min_expr,
-        max_val=args.max_val,
-        dry_run=args.dry_run,
-        zscore_cutoff=args.zscore_cutoff,
-        index_col=args.index_col
-    )
-    print("Build successful!")
-    print(cob.summary())
+        # Check to see if this dataset is already built
+        if available_datasets('Expr',args.name):
+            print('Warning! This dataset has already been built.')
+            co.Tools.del_dataset('Expr',args.name,force=args.force)
+            
+        # Basically just pass all the CLI arguments to the COB class method  
+        cob = co.COB.from_table(
+            args.filename,
+            args.name,
+            args.description,
+            refgen,
+            # Optional arguments
+            sep=args.sep,
+            rawtype=args.rawtype,
+            # Data Processing
+            quality_control=quality_control,
+            normalization=normalize,
+            quantile=args.quantile,
+            # Data processing parameters
+            max_gene_missing_data=args.max_gene_missing_data,
+            max_accession_missing_data=args.max_accession_missing_data,
+            min_single_sample_expr=args.min_single_sample_expr,
+            min_expr=args.min_expr,
+            max_val=args.max_val,
+            dry_run=args.dry_run,
+            zscore_cutoff=args.zscore_cutoff,
+            index_col=args.index_col
+        )
+        print("Build successful!")
+        print(cob.summary())
+    except Exception as e:
+        print("Build failed. Rolling back: removing corrupted files...")
+        co.Tools.del_dataset('Expr',args.name,force=True)
+        raise e
 
 def build_refgen(args):
     co.RefGen.from_gff(
