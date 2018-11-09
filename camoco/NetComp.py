c61c4253f834ff66e73024f54a9261127311f0c6
rob <rob@linkage.io>
2018-11-09 14:42:24 -0700

    implement a decorator for handling iterables

--- camoco/NetComp.py
+++ camoco/NetComp.py
@@ -1,6 +1,35 @@
-from .COB import COB
+import types
+import six
+import random
+import pandas as pd
+import logging
 
-class NetComp(Camoco):
+from .COB import COB
+from .Camoco import Camoco
+
+from functools import wraps 
+from collections.abc import Iterable
+from itertools import permutations
+from minus80 import Freezable
+from contextlib import contextmanager
+
+
+def accepts_iterable(fn):
+    '''
+    This decorator detects when an iterable is passed into the method call
+    instead of a single element (first arg only). Then, instead of calling the
+    function on the arg (normally) the method is applied to each element of the
+    iterable. This essentially turns fn(arg) into [f(x) for x in arg]. 
+    '''
+    @wraps(fn)
+    def wrapped(self,arg,*args,**kwargs):
+        # 
+        if (isinstance(arg,Iterable) and not \
+            isinstance(arg, six.string_types)):
+            return [fn(self,x,*args,**kwargs) for x in arg]      
+        else:
+            fn(self,arg,*args,**kwargs)
+    return wrapped
 
     def __init__(self,name,networks):
         self.networks = set()
