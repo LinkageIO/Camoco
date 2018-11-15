76784e79d20a1b19c7521c8299d02feb5834ede4
rob <rob@linkage.io>
2018-11-14 18:54:38 -0700

    change themes

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.programoutput',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
