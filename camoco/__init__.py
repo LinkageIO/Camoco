adb3ddcd10e88655bb33a2c4a2ca44c2c3043d87
rob <rob@linkage.io>
2019-01-08 08:21:58 -0700

    get rid of comments

--- camoco/__init__.py
+++ camoco/__init__.py
@@ -44,7 +44,6 @@ pyximport.install(setup_args={
 })
 
 import matplotlib
-#matplotlib.use('Agg')
 
 
 from .Config import cf
@@ -52,15 +51,10 @@ from .Camoco import Camoco
 from .Expr import Expr
 from .COB import COB
 from .RefGen import RefGen
-#from .RefGenDist import *
-#from .PCCUP import *
 
 from .Ontology import Ontology,Term
 from .GWAS import GWAS
 from .Locus import Locus
-#from .Tools import available_datasets,del_dataset
-#from .Tools import mv_dataset,redescribe_dataset
-#from .GEO import Family
 from .GOnt import GOnt
 from .Overlap import Overlap
 
