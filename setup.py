ec970c6b7aaddcc4754bba19eac89fe4ecf1b943
rob <rob@linkage.io>
2019-08-07 09:40:50 -0600

    remove python 3.7 blocker from setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     package_data = {
         '':['*.cyx']    
     },
-    python_requires='>=3.6, <3.7',
+    python_requires='>=3.6',
     setup_requires = [
         # Setuptools 18.0 properly handles Cython extensions.
         'setuptools>=18.0',
