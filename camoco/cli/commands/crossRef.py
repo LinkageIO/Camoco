235e3ea43f1c3e4b3e2aa40479da262a6f2d9c46
rob <rob@linkage.io>
2020-02-13 09:56:46 -0700

    remove uneecassary files for v1 release

--- camoco/cli/commands/crossRef.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import camoco as co
-import matplotlib.pylab as plt
-
-
-def crossref(args):
-    cobs = [co.COB(x) for x in args.cobs]
--- camoco/cli/commands/remove.py
+++
@@ -1,7 +0,0 @@
-import camoco as co
-from camoco.Tools import del_dataset
-
-
-def remove(args):
-    del_dataset(args.type, args.name, force=args.force)
-    print("Done")
