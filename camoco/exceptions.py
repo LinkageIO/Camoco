b6ff033035550f2f476a779335844ca33c7ceb6f
Rob <rob@linkage.io>
2020-02-13 15:35:36 -0700

    add coexexists error

--- camoco/exceptions.py
+++ camoco/exceptions.py
@@ -2,5 +2,8 @@ class CamocoError(Exception):
     def __init__(self,msg=''):
         self.message=msg
 
-class CoexNetEmptyError(CamocoError):
+class CoexExistsError(CamocoError):
+    pass
+
+class CoexEmptyError(CamocoError):
     pass
