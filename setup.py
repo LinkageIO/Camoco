a0efee140a10576aa8163243b3de0e57d5e4699d
rob <rob@linkage.io>
2020-02-13 09:58:09 -0700

    trim down deps for v1, add as needed

--- setup.py
+++ setup.py
@@ -34,11 +34,6 @@ pccup = Extension(
     sources=['camoco/PCCUP.pyx'],
     include_dirs=[numpy.get_include()]
 )
-refgendist = Extension(
-    'camoco.RefGenDist',
-    sources=['camoco/RefGenDist.pyx'],
-    include_dirs=[numpy.get_include()]
-)
 
 # Check if running Windows
 if os.name == 'nt':
@@ -50,42 +45,36 @@ setup(
     version = find_version('camoco','__init__.py'),
     packages = find_packages(),
     scripts = [
-        'camoco/cli/camoco'
     ],
-    ext_modules = [pccup,refgendist],
+    ext_modules = [pccup],
     cmdclass = {
     },
     package_data = {
         '':['*.cyx']    
     },
-    python_requires='>=3.6,<3.9',
+    python_requires='>=3.8',
     setup_requires = [
         # Setuptools 18.0 properly handles Cython extensions.
-        'setuptools>=18.0',
-        #'numpy==1.14.5',
-        'cython>=0.29.10',
+        'setuptools>=45.1.0',
+        'numpy==1.18.1',
+        'cython>=0.29.13',
     ],
     install_requires = [		
-        'minus80>=0.3.3',
-       #'cython>=0.29.10',    		
-        'igraph==0.1.11',		
-        'pyyaml>=5.1.1',
-        'matplotlib>=3.1.0',		
-        'numpy>=1.16.4',		
-        'scipy>=1.2.1',		
-        'pandas==0.23.4',		
-        'fa2>=0.3.5',
-        'scikit-learn>=0.21.2',
-        'statsmodels>=0.8.0',
-        'termcolor>=1.1.0',
-        'powerlaw>=1.4.6',
-        'flask==1.0.3',
-        'markov-clustering>=0.0.6.dev0',
-        'fastcluster>=1.1.25',
-        'bcolz>=1.2.1',
-        'psutil>=5.6.3',
-        'odo>=0.5.0',
-        'blaze>=0.10.1'
+        'minus80>=1.0.0',
+        'locuspocus>=1.0.2',
+        'tinymongo>=0.2.0',
+       #'matplotlib>=3.1.3',		
+       #'scipy>=1.4.1',		
+       #'pandas>=1.0.1',		
+       #'fa2>=0.3.5',
+       #'scikit-learn>=0.21.2',
+       #'statsmodels>=0.8.0',
+       #'termcolor>=1.1.0',
+       #'powerlaw>=1.4.6',
+       #'flask==1.0.3',
+       #'markov-clustering>=0.0.6.dev0',
+       #'fastcluster>=1.1.25',
+       #'psutil>=5.6.3',
     ],
     extras_require={
         'docs' : [
@@ -98,8 +87,6 @@ setup(
         ]
     },
     dependency_links = [
-        #'git+https://github.com/LinkageIO/Minus80'
-        #'git+https://github.com/rogerbinns/apsw' 
     ],
     include_package_data=True,
     author = 'Rob Schaefer',
