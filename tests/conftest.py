cd9d608d146a489ee88afbb91fe3d8f8c7f0191d
rob <rob@linkage.io>
2020-02-14 10:21:08 -0700

    add more to from_DataFrame (incomplete)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 
 import pytest
 
+import camoco as co
+import pandas as pd
 import minus80 as m80
 import locuspocus as lp
 
@@ -12,7 +14,10 @@ from pathlib import Path
 def Zm5bFGS():
     if not m80.Tools.available('Loci','Zm5bFGS'):
         # Create the refgen object
-        loci = lp.Loci.from_gff('Zm5bFGS','raw/RefGen/ZmB73_5b_FGS.gff.gz')
+        loci = lp.Loci.from_gff(
+            'Zm5bFGS',
+            'raw/RefGen/ZmB73_5b_FGS.gff.gz'
+        )
     else:
         # Load it
         loci = lp.Loci('Zm5bFGS')
@@ -22,10 +27,20 @@ def Zm5bFGS():
 @pytest.fixture(scope='module')
 def ZmTissue(Zm5bFGs):
     if not m80.Tools.available('CoexNet','ZmTissue'):
+        # Get the pd DataFrame
+        df = pd.read_table(
+            'raw/Expr/RNASEQ/MaizeRNASeqTissue.tsv.bz2'
+        )
         # Create the refgen object
-        loci = lp.Loci.from_DataFrame(
-            'Zm5bFGS',
-            'raw/RefGen/ZmB73_5b_FGS.gff.gz'
+        ZmTissue = co.Coex.from_DataFrame(
+            'ZmTissue',
+            'Maize RNASeq Tissue Atlas Network, Sekhon 2013, PLoS ONE',
+            df,
+            Zm5bFGS,
+            min_expr=0.001,
+            max_locus_missing_data=0.3,
+            max_accession_missing_data=0.08,
+            min_single_accession_expr=1,
         )
     else:
         # Load it
