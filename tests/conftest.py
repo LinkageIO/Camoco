9c8de0a7161f4c9a078096e080085b375034997e
Rob <rob@linkage.io>
2021-01-16 11:55:38 -0700

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,12 @@ import pandas as pd
 import minus80 as m80
 import locuspocus as lp
 
-from pathlib import Path
 
 # Create a Loci object from the Zm5b Refgen
 
 @pytest.fixture(scope='module')
 def Zm5bFGS():
-    if not m80.Tools.available('Loci','Zm5bFGS'):
+    if not m80.exists('Loci','Zm5bFGS'):
         # Create the refgen object
         loci = lp.Loci.from_gff(
             'Zm5bFGS',
@@ -26,7 +25,7 @@ def Zm5bFGS():
 
 @pytest.fixture(scope='module')
 def ZmTissue(Zm5bFGs):
-    if not m80.Tools.available('Coex','ZmTissue'):
+    if not m80.exists('Coex','ZmTissue'):
         # Get the pd DataFrame
         df = pd.read_table(
             'raw/Expr/RNASEQ/MaizeRNASeqTissue.tsv.bz2'
